a9ba862acc0d5b84d7cf926dec43dbe6002ca344
V L <agesofheaven@gmail.com>
2016-01-09 22:47:45 -0500

    Reformat some things to bugfix

--- scripts/static_state_machine.py
+++ scripts/static_state_machine.py
@@ -4,17 +4,54 @@ import smach
 import smach_ros
 import rospy
 from threading import Thread
-from planner.tasks import *
+#from planner import tasks
+from smach_ros import SimpleActionState
+
+
+
+
+
+
+#condition callback for idle since it is a condition state
+#TODO: setup to condition on a topic instead
+
+def idle_cond_cb(ud):
+    check = raw_input()
+    if check == 1:
+        return True
+    else:
+        return False
+    
+
+#callbacks for move, parameter reading will be moved out of the goal callback for an easier time
+#(only the first and last 3 lines of that callback will remain)
+def move_goal_cb(userdata,goal):
+    uid=userdata.move_id
+    
+    params = rospy.get_param('~move'+'id')
+    time = RosTime(params.time[0],params.time[1])
+    velocity = Twist(Vector3(params.velocity[0],params.velocity[1],params.velocity[2]),Vector3(params.velocity[3],params.velocity[4],params.velocity[5]))
+    move_goal = MoveGoal(time,velocity,theta,depth)
+    move_goal = userdata.movelist[uid]
+    userdata.move_id += 1
+    return move_goal
+#TODO: make this callback less limited and allow more arbitrary IDs
+def move_result_cb(userdata,status,goal):
+    if status == GoalStatus.SUCCEEDED:
+        if userdata.move_id==4:
+            userdata.move_id = 0
+            return 'done'
 
 def create_machine():
     static_sm = smach.StateMachine(
         outcomes=['succeeded','aborted','preempted'])
     
     with static_sm:
-        countdown = rospy.getparams('~countdown')
+        #countdown = rospy.get_param('countdown')
+        countdown = rospy.Time(10)
         
         smach.StateMachine.add('Idle',
-                               smach_ros.ConditionState(tasks.idle_cond_cb,poll_rate=rospy.Duration(1),max_checks=1),
+                               smach_ros.ConditionState(idle_cond_cb,poll_rate=rospy.Duration(1),max_checks=1),
                                transitions={'true':'Initialize',
                                             'false':'Idle'})
         smach.StateMachine.add('Initialize',
